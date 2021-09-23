0e7d848cea82689cb7893636c7675d445bec58c8
Teymur Zeynalov <teymur.zeynalov@outlook.com>
2021-09-23 18:12:43 +0400

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aaaaa <code>src/App.js</code> 
+          bbbbb <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
